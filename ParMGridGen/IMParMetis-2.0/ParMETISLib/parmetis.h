a3fad7d61e1201aa99c0c902375fe92db5cc9595
Betameche <meven.mognol@gmail.com>
2019-08-05 13:46:11 +0200

    Copy of mrklein git

---
+++ ParMGridGen/IMParMetis-2.0/ParMETISLib/parmetis.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2001, Regents of the University of Minnesota
+ *
+ * parmetis.h
+ *
+ * This file includes all necessary header files
+ *
+ * George Irene
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdarg.h>
+#include <time.h>
+#include <mpi.h>
+
+#ifdef DMALLOC
+#include <dmalloc.h>
+#endif
+
+#include "rename.h"
+#include "defs.h"
+#include "struct.h"
+#include "macros.h"
+#include "proto.h"
+
